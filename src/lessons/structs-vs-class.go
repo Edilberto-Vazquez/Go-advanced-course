5202e72c31b85a8f08ed02b9349c0a8fdcac559d
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-08-29 21:19:34 -0500

    methods and functions

--- src/lessons/structs-vs-class.go
+++ src/lessons/structs-vs-class.go
@@ -7,12 +7,31 @@ type Employee struct {
 	name string
 }
 
+func (e *Employee) Setid(id int) {
+	e.id = id
+}
+
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+func (e *Employee) Getid() int {
+	return e.id
+}
+
+func (e *Employee) GetName() string {
+	return e.name
+}
+
 func MakeObject() {
 	e := Employee{}
-	e2 := Employee{id: 2, name: "Maria"}
 	fmt.Printf("%v\n", e)
 	e.id = 1
 	e.name = "Edilberto"
 	fmt.Printf("%v\n", e)
-	fmt.Printf("%v\n", e2)
+	e.Setid(5)
+	e.SetName("Maria")
+	fmt.Printf("%v\n", e)
+	fmt.Println(e.Getid())
+	fmt.Println(e.GetName())
 }
