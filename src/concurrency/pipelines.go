2b920fe07e8edbe794945e9858d77cdb457751b8
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-08-31 21:47:04 -0500

    Defining read and write channels

---
+++ src/concurrency/pipelines.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// c chan<- int write vars enter the channel channel
+// c <-chan int read vars leave the channel  channel
+
+func Generator(c chan<- int) {
+	for i := 0; i < 10; i++ {
+		c <- i
+	}
+	close(c)
+}
+
+func Double(in <-chan int, out chan<- int) {
+	for v := range in {
+		out <- 2 * v
+	}
+	close(out)
+}
+
+func Print(c <-chan int) {
+	for v := range c {
+		fmt.Printf("Id %d\n", v)
+	}
+}
+
+func main() {
+	generator := make(chan int)
+	double := make(chan int)
+
+	go Generator(generator)
+	go Double(generator, double)
+	Print(double)
+}
--- src/concurrency/sem.go
+++ src/concurrency/sem.go
@@ -1,26 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+// func doSomething(i int, wg *sync.WaitGroup, c chan int) {
+// 	defer wg.Done()
+// 	fmt.Printf("Id %d started\n", i)
+// 	time.Sleep(1 * time.Second)
+// 	fmt.Printf("Id %d finished\n", i)
+// 	<-c
+// }
 
-func doSomething(i int, wg *sync.WaitGroup, c chan int) {
-	defer wg.Done()
-	fmt.Printf("Id %d started\n", i)
-	time.Sleep(1 * time.Second)
-	fmt.Printf("Id %d finished\n", i)
-	<-c
-}
-
-func main() {
-	c := make(chan int, 10)
-	var wg sync.WaitGroup
-	for i := 0; i < 10; i++ {
-		c <- 1
-		wg.Add(1)
-		go doSomething(i, &wg, c)
-	}
-	wg.Wait()
-}
+// func main() {
+// 	c := make(chan int, 10)
+// 	var wg sync.WaitGroup
+// 	for i := 0; i < 10; i++ {
+// 		c <- 1
+// 		wg.Add(1)
+// 		go doSomething(i, &wg, c)
+// 	}
+// 	wg.Wait()
+// }
