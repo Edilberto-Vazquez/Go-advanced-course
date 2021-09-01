6b60df305a8468d7cc28b61118ae798e32e755dd
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-08-31 20:32:42 -0500

    buffered and unbuffered chanel

---
+++ src/concurrency/channels.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// unbuffered channel
+	cU := make(chan int)
+
+	cU <- 1
+
+	fmt.Println(<-cU)
+
+	// buffered channel
+	cB := make(chan int, 3)
+
+	cB <- 1
+	cB <- 2
+	cB <- 3
+
+	fmt.Println(<-cB)
+	fmt.Println(<-cB)
+	fmt.Println(<-cB)
+}
